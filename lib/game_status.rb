f7ed310ac7b4b2b22a5b8046f622e35aa3850380
Philip Warren <philwarren1@gmail.com>
2020-07-19 14:32:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,7 +36,6 @@ def won?(board)
      position_1 == "O" && position_2 == "O" &&
      position_3 == "O" 
      return win_combination
-    elsif
      position_1 == " " && position_2 == " " &&
      position_3 == " "
      return false
